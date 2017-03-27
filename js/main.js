f7f43326c1184ac11de17cad694be8e2de70b613
1boeman <joriso@hardcode.nl>
2017-03-28 00:05:50 +0200

    editor

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-(function(){
+(function() {
 
   var c = document.getElementById("hetCanvas");
-  var ctx = c.getContext('2d')
-  var editor = (function(ctx){
+  var offLeft = c.offsetLeft,
+      offTop = c.offsetTop;
+  var ctx = c.getContext('2d');
+
+  
+
+  var Pixel = function(){
+    this.commit = function(x,y,ctx){
+      ctx.fillStyle="#FF0000";
+      ctx.fillRect(x,y,10,10);
+    }
+  };
+  var tool = new Pixel();
+
+
     
 
-     
+  
+ 
+  var editor = (function(ctx,c,tool) {
+    $(c).click(function(e){
+      var x = e.pageX - offLeft,
+          y = e.pageY - offTop;
+
+      tool.commit(x,y,ctx);
+    });
+
+    
 
     var pub = {
       resize:function(w,h){
@@ -14,45 +37,41 @@
       }
     }    
     return pub; 
-  })(ctx);
-
+  })(ctx,c,tool);
   
+  var controls = (function(editor) {
+    var imgData; 
+    var copy = function(){
+      imgData = ctx.getImageData(0,0,c.width,c.height);
+    };
 
-  var controls = (function(editor){
+    var paste = function(){
+      ctx.putImageData(imgData, 0,0)
+    };
+    
     var handlers = {
-      setWidth : function(){
-        c.width = this.value   
+      
+      setWidth : function() {
+        copy();
+        c.width = this.value;
+        paste();
       },
 
-      setHeight : function(){
+      setHeight : function() {
+        copy();
         c.height = this.value; 
+        paste();
       }
     };
 
 
-    $('input.control').change(function(){
+    $('input.control').change(function(){ 
       handlers[$(this).data('handle')].apply(this);
     });  
 
 
 
   })(editor);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
